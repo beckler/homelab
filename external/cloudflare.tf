e3574076a8c8fe264ce188048efed0ebeb94ccd1
Michael Beckler <beckler@hey.com>
2022-02-09 23:14:22 -0500

    updated a lot of stuff

--- external/cloudflare.tf
+++ external/cloudflare.tf
@@ -1,5 +1,5 @@
-data "cloudflare_zone" "khuedoan_com" {
-  name = "khuedoan.com"
+data "cloudflare_zone" "adc_io" {
+  name = "adc.io"
 }
 
 data "cloudflare_api_token_permission_groups" "all" {}
--- external/versions.tf
+++ external/versions.tf
@@ -1,12 +1,11 @@
 terraform {
   required_version = "~> 1.1.0"
 
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "khuedoan"
+  cloud {
+    organization = "beckler"
 
     workspaces {
-      name = "homelab-external"
+      name = "k3s-bare-metal-cluster"
     }
   }
 
@@ -16,10 +15,10 @@ terraform {
       version = "~> 3.4.0"
     }
 
-    b2 = {
-      source  = "Backblaze/b2"
-      version = "~> 0.7.0"
-    }
+    # b2 = {
+    #   source  = "Backblaze/b2"
+    #   version = "~> 0.7.0"
+    # }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -38,10 +37,10 @@ provider "cloudflare" {
   api_key = var.cloudflare_api_key
 }
 
-provider "b2" {
-  application_key_id = var.b2_application_key_id
-  application_key    = var.b2_application_key
-}
+# provider "b2" {
+#   application_key_id = var.b2_application_key_id
+#   application_key    = var.b2_application_key
+# }
 
 provider "kubernetes" {
   # Use KUBE_CONFIG_PATH environment variables
